cf2f4319b3c67c2382ac152f4d5d663a9977f1f2
Ubuntu <ubuntu@ip-172-31-80-95.ec2.internal>
2019-09-12 19:02:04 +0000

    FIxed some Errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -299,50 +299,53 @@ function showInfoWindow() {
   });
 }
 
-// Load the place information into the HTML elements used by the info window.
-function buildIWContent(place) {
-  document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' + 'src="' + place.icon + '"/>';
-  document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url + '">' + place.name + '</a></b>';
-  document.getElementById('iw-address').textContent = place.vicinity;
 
-  if (place.formatted_phone_number) {
-    document.getElementById('iw-phone-row').style.display = '';
-    document.getElementById('iw-phone').textContent =
-        place.formatted_phone_number;
-  } else {
-    document.getElementById('iw-phone-row').style.display = 'none';
-  }
-}  
+      // Load the place information into the HTML elements used by the info window.
+      function buildIWContent(place) {
+        document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
+            'src="' + place.icon + '"/>';
+        document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
+            '">' + place.name + '</a></b>';
+        document.getElementById('iw-address').textContent = place.vicinity;
 
+        if (place.formatted_phone_number) {
+          document.getElementById('iw-phone-row').style.display = '';
+          document.getElementById('iw-phone').textContent =
+              place.formatted_phone_number;
+        } else {
+          document.getElementById('iw-phone-row').style.display = 'none';
+        }
 
-// Assign a five-star rating to the hotel, using a black star ('&#10029;')
-// to indicate the rating the hotel has earned, and a white star ('&#10025;')
-// for the rating points not achieved.
-if (place.rating) {
-  var ratingHtml = '';
-  for (var i = 0; i < 5; i++) {
-    if (place.rating < (i + 0.5)) {
-      ratingHtml += '&#10025;';
-    } else {
-      ratingHtml += '&#10029;';
-    }
-  document.getElementById('iw-rating-row').style.display = '';
-  document.getElementById('iw-rating').innerHTML = ratingHtml;
-  }
-} else {
-    document.getElementById('iw-rating-row').style.display = 'none';
-  }
-// The regexp isolates the first part of the URL (domain plus subdomain
-// to give a short URL for displaying in the info window.
-if (place.website) {
-  var fullUrl = place.website;
-  var website = hostnameRegexp.exec(place.website);
-  if (website === null) {
-    website = 'http://' + place.website + '/';
-    fullUrl = website;
-    }
-    document.getElementById('iw-website-row').style.display = '';
-    document.getElementById('iw-website').textContent = website;
-    } else {
-      document.getElementById('iw-website-row').style.display = 'none';
+        // Assign a five-star rating to the hotel, using a black star ('&#10029;')
+        // to indicate the rating the hotel has earned, and a white star ('&#10025;')
+        // for the rating points not achieved.
+        if (place.rating) {
+          var ratingHtml = '';
+          for (var i = 0; i < 5; i++) {
+            if (place.rating < (i + 0.5)) {
+              ratingHtml += '&#10025;';
+            } else {
+              ratingHtml += '&#10029;';
+            }
+          document.getElementById('iw-rating-row').style.display = '';
+          document.getElementById('iw-rating').innerHTML = ratingHtml;
+          }
+        } else {
+          document.getElementById('iw-rating-row').style.display = 'none';
+        }
+
+        // The regexp isolates the first part of the URL (domain plus subdomain)
+        // to give a short URL for displaying in the info window.
+        if (place.website) {
+          var fullUrl = place.website;
+          var website = hostnameRegexp.exec(place.website);
+          if (website === null) {
+            website = 'http://' + place.website + '/';
+            fullUrl = website;
+          }
+          document.getElementById('iw-website-row').style.display = '';
+          document.getElementById('iw-website').textContent = website;
+        } else {
+          document.getElementById('iw-website-row').style.display = 'none';
+        }
       }
